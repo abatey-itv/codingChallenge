ada8934db5d1b6d424c0dff2d697ba46583944c1
polyglotpiglet <alexandra.p.batey@gmail.com>
2020-05-31 11:16:49 +0100

    refactor try to catch all 3rd party errors

--- src/main/scala/validator/ChecksumRequestValidator.scala
+++ src/main/scala/validator/ChecksumRequestValidator.scala
@@ -4,28 +4,30 @@ import java.io.File
 
 import validator.MD5Checker.calculateChecksum
 
+import scala.util.Try
+
 object ChecksumRequestValidator {
 
   final case class Request(file: File, expectedChecksum: String)
 
   sealed trait Result
+
   final case class Valid(file: File, checksum: String) extends Result
+
   final case class ChecksumMismatch(file: File, expectedChecksum: String, actualChecksum: String) extends Result
+
   final case class RequestError(file: File, expectedChecksum: String) extends Result
 
   def validate(requests: Seq[Request]): Seq[Result] = requests.map(req => validateChecksum(req))
 
   private def validateChecksum(request: Request): Result = {
-    if (!request.file.exists())
-      RequestError(request.file, request.expectedChecksum)
-    else {
-      // todo wrap in a try?
+    Try({
       val actualChecksum = calculateChecksum(request.file.getAbsolutePath)
       if (actualChecksum == request.expectedChecksum)
         Valid(request.file, actualChecksum)
       else
         ChecksumMismatch(request.file, request.expectedChecksum, actualChecksum)
-    }
+    }).getOrElse(RequestError(request.file, request.expectedChecksum))
   }
 
 }
